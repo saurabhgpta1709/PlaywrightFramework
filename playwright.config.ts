f427d93882931371b2aa0a7d46ca2ef4abe9564f
saurabhgupta <saurabh.gpta1709@gmail.com>
2026-09-12 21:37:16 +0530

    Move Playwright tests into e2e folder

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="node" />
+
 import { defineConfig, devices } from '@playwright/test';
 
 /**
@@ -12,7 +14,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: './e2e/tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -30,6 +32,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    navigationTimeout: 10000,
   },
 
   /* Configure projects for major browsers */
